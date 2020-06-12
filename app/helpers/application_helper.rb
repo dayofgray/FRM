2ec27e7a8e3dcee93d76b631533f660db18e061d
dayofgray <graysonpberger@gmail.com>
2020-06-11 23:56:21 -0400

    Add login options helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def login_options
+        if logged_in?
+            link_to "Logout", logout_path
+        else
+            link_to "Login", login_path
+        end
+    end
 end
